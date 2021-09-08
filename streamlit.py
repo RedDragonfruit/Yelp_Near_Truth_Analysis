e7dd94ec4c7cc23efb5101911c5601924f91c01a
RedDragonfruit <83319308+RedDragonfruit@users.noreply.github.com>
2021-09-08 16:56:21 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,6 @@
 import streamlit as st
+
+url = ' https://drive.google.com/drive/folders/1gNl3vReXZBquEp-EeYWjLcNJmTYTpo1Z'
+path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+df = pd.read_csv(path)
+

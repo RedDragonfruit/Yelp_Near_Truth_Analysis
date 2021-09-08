8fe8df12e8decd2a403348327faeb903afff4694
RedDragonfruit <83319308+RedDragonfruit@users.noreply.github.com>
2021-09-08 17:05:52 -0400

    Delete streamlit.py

--- streamlit.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-url = ' https://drive.google.com/drive/folders/1gNl3vReXZBquEp-EeYWjLcNJmTYTpo1Z'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
-

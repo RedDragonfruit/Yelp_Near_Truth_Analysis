eb89439b1f44df751edd9da6d4a34eade2ef5914
RedDragonfruit <83319308+RedDragonfruit@users.noreply.github.com>
2021-09-11 17:47:45 -0400

    Add files via upload

--- Tools/FNC.py
+++ Tools/FNC.py
@@ -7,6 +7,8 @@ import string
 import re
 import matplotlib.pyplot as plt
 import seaborn as sns
+import nltk
+from sklearn.metrics import plot_confusion_matrix
 
 def text_cleaning(text):
     '''
@@ -53,3 +55,76 @@ def sns_barplot(x,y=None,df=None,start = 1,grid = True, title = None, x_label = 
         i+=1
     
     plt.xticks(count,labels, size = 15);
+
+
+#function to determine the age of a user in years
+
+def account_length(df , yr_col = "year"):
+    grp_data = df.groupby(['user_id','year']).count()
+    
+    user = 0
+    mini = 200000
+    maxi = 0
+    
+    user_record = []
+    year_record = []
+
+    for x in grp_data.index:
+        #
+        if user == x[0]:
+            if int(x[1]) <= int(mini):
+                mini = int(x[1])
+            elif int(x[1]) > int(maxi):
+                maxi = int(x[1])
+
+            
+        if user != x[0]:
+            
+            if user != 0:
+                year_record.append((int(maxi)-int(mini)))
+                user_record.append(int(user))
+                user = int(x[0])
+                mini = int(x[1])
+                maxi = int(x[1])
+
+            else:
+                user = int(x[0])
+                mini = int(x[1])
+                maxi = int(x[1])
+           
+    return user_record, year_record
+
+
+
+punct = set(string.punctuation)
+
+def tokenize_text(text):
+    # remove numbers
+    text_nonum = re.sub(r'\d+', '', text)
+    # remove punctuations and convert characters to lower case
+    text_nopunct = "".join([char.lower() for char in text_nonum if char not in punct]) 
+    # substitute multiple whitespace with single whitespace
+    # Also, removes leading and trailing whitespaces
+    text_no_doublespace = re.sub('\s+', ' ', text_nopunct).strip()
+    return nltk.word_tokenize(text_no_doublespace)
+
+
+
+def remove_short_revs(df,col = 'review'):
+    indexNames = []
+    sub_5_reviews = []
+#enumerate through the cloned_df reviews and find any reviews with less than 15 words
+#this is important as there were reviews with simply characters and one words.
+    for i,rev in enumerate(df[col]):
+        if len(rev)<15:
+            indexNames.append(i)
+            sub_5_reviews.append(rev)
+        
+    return df.drop(indexNames)
+
+
+
+def score(model, X = None, y = None, text = 'Train'):
+    model.fit(X,y)
+    print(f'{text} score',model.score(X,y))
+    plot_confusion_matrix(model,X,y)
---
+++ Tools/NN.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, confusion_matrix
+import nltk
+import re
+
+from nltk.corpus import stopwords
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import one_hot
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+from tensorflow.keras.layers import Dense, Embedding,GlobalMaxPooling1D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Embedding, Conv1D
+
+
+
+def clean_data(review):
+    review = re.sub('[^a-zA-Z]', ' ',review)
+    review = review.lower()
+    return review
+
+sw = stopwords.words('english')
+sw += ['food','place','good',"great"]
+
+def remove_stop_words(review):
+    review_minus_sw = []
+    stop_words = sw
+    review = review.split()
+    review = [review_minus_sw.append(word) for word in review if word not in stop_words]
+    review = ' '.join(review_minus_sw)
+    return review
+
+
+###################################################### 
+def NN_cleaning(original_df):
+    
+    original_df['review'] = original_df['review'].apply(clean_data)
+    original_df['review'] = original_df['review'].apply(remove_stop_words)
+    corpus = list(original_df['review'])
+    cv = CountVectorizer(max_features = 1000)
+    X = cv.fit_transform(corpus).toarray()
+    y = original_df['target'].values
+    tf_transformer = TfidfTransformer()
+    X = tf_transformer.fit_transform(X).toarray()
+    tfidfVectorizer = TfidfVectorizer(max_features =1000)
+    X = tfidfVectorizer.fit_transform(corpus).toarray()
+    docs = original_df['review']
+    labels = original_df['target']
+    vocab_size = 5000
+
+    docs = [one_hot(d, vocab_size,filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~',lower=True, split=' ') for d in docs]
+
+    max_length = 100
+    docs = pad_sequences(docs, maxlen=max_length, padding='post')
+    return docs, labels
+
+
+
+def text_to_fn(text):
+    original_df = pd.DataFrame([text], columns = ['review'])
+    return original_df
+
+
+def NN_cleaning_st(text):
+    
+    original_df = pd.DataFrame([text], columns = ['review'])
+    original_df['review'] = original_df['review'].apply(clean_data)
+    
+    original_df['review'] = original_df['review'].apply(remove_stop_words)
+    corpus = list(original_df)
+    cv = CountVectorizer(max_features = 1000)
+    X = cv.fit_transform(corpus).toarray()
+
+    tf_transformer = TfidfTransformer()
+    X = tf_transformer.fit_transform(X).toarray()
+    tfidfVectorizer = TfidfVectorizer(max_features =1000)
+    X = tfidfVectorizer.fit_transform(corpus).toarray()
+    docs = original_df
+    vocab_size = 5000
+    docs = [one_hot(d, vocab_size,filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~',lower=True, split=' ') for d in docs]
+    max_length = 100
+    docs = pad_sequences(docs, maxlen=max_length, padding='post')
+    return docs
